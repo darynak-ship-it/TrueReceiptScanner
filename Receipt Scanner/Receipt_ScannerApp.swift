78413854b2466814af9c1e4e14bf55bd65cbf354
Daryna Kalnichenko <darynakalnichenko@gmail.com>
2025-10-15 17:58:26 +0100

    Initial Commit

---
+++ Receipt Scanner/Receipt_ScannerApp.swift
@@ -0,0 +1,21 @@
+//
+//  Receipt_ScannerApp.swift
+//  Receipt Scanner
+//
+//  Created by Daryna Kalnichenko on 10/15/25.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct Receipt_ScannerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
